1ffa1b481b08ce2907508f99234a97ae00a89a55
Vladislav <vladislav.andreev.2017@mail.ru>
2024-07-05 00:31:37 +0300

    Delete homework4.py

--- homework4.py
+++
@@ -1,9 +0,0 @@
-my_string = input("Напишите слово, которое сейчас пришло вам в голову: ")
-print("Количество символов введенного текста:", len(my_string))
-print("Я верхний регистр: ", my_string. upper())
-print("Я нижний регистр: ", my_string.lower())
-print("У меня нет пробелов: ", my_string.replace(" ", ""))
-a = my_string[0]
-print("Первый символ строки: ", a)
-b = my_string[-1]
-print("Последний символ строки: ", b)
